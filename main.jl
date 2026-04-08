772fa22062115a6dc22b241689f0d72a374a064a
san <jensonianbrown@gmail.com>
2026-04-07 21:10:15 -0400

    tuesday

--- main.jl
+++ main.jl
@@ -4,33 +4,88 @@ begin
 
     using Revise
     using Ollamajl
+    using JSON3
     # using .Ollamajl
 end
 
-# Ollama instance
+# TODO test tool calling / tool creation
+
+# Instantiate an ollama instance
 o = OllamaInstance("http://127.0.0.1:11434")
 
 # Generate a response
-generateOpts = Options(model="gemma4:e2b", prompt="Test message", stream=false)
+# ------------
 
-g = generate(o, generateOpts)
+# Using kwargs (simplest)
+generate(o, model="gemma4:e2b", prompt="Go gators!", stream=false)
 
-# With kwargs
-g2 = generate(o; model="gemma4:e2b", prompt="hello robot")
+# Using a premade Options object
+opts = Options(
+    model="gemma4:e2b", 
+    prompt="Go gators!", 
+    stream=false
+    )
+
+generate(o, opts)
 
 # Generate the next chat message
-messages=[Dict("role" => "user", "content" => "hello robot")]
+# ------------
 
-c2 = chat(o; model="gemma4:e2b", messages=messages, stream=false)
+# Using kwargs (simplest) by passing a vector of Dicts
+chat(o,
+    model="gemma4:e2b", 
+    messages=[Dict("role" => "user", "content" => "Go gators!")],
+    stream=false
+)
 
-# With kwargs
-messages=[Dict("role" => "user", "content" => "hello robot")]
+# Using Message struct
+messages = Message(role="user", content="Go gators!")
+chat(o; model="gemma4:e2b", messages=[messages], stream=false)
 
-chatOpts = Options(model="gemma4:e2b", messages=messages, stream=false)
+# Using JSON3 to read and pass raw JSON
+rawJson = Dict("role" => "user", "content" => "Go gators!") |> JSON3.write
+messages = JSON3.read(rawJson)
 
-c = chat(o, chatOpts)
+chat(o,
+    model="gemma4:e2b",
+    messages = [messages],
+    stream=false
+)
+
+# Using premade Options object
+msgs = Message(role="user", content="Go gators!")
+opts = Options(
+    model="gemma4:e2b",
+    messages=[msgs],
+    stream=false
+    )
+
+chat(o, opts)
+
+# Message struct can also be passed to Options struct
+msgs = Message(
+    role="user",
+    content="Go gators!"
+    )
+
+opts = Options(
+    model="gemma4:e2b", 
+    messages=[msgs], 
+    stream=false
+    ) 
+
+resp = chat(o, opts)
+
+# The Options struct is mutable to allow storage of messages from the LLM
+llmMsg = String(resp.body) |> JSON3.read
+push!(opts.messages, Message(role=llmMsg.message.role, content=llmMsg.message.content))
 
 # Embeddings
-embedOpts = Options(model="embeddinggemma", input="why is the sky blue?")
-e = embed(o, embedOpts)
-e2 = embed(o; model="embeddinggemma", input="why is the sky blue?")
+# ------------
+
+# Using kwargs
+embed(o; model="embeddinggemma", input="why is the sky blue?")
+
+# Using Options struct
+opts = Options(model="embeddinggemma", input="why is the sky blue?")
+e = embed(o, opts)
--- src/Ollamajl.jl
+++ src/Ollamajl.jl
@@ -2,7 +2,7 @@ module Ollamajl
 
 using HTTP, JSON3, StructTypes
 
-export OllamaInstance, Options, initInstance, generate, chat, embed, tags
+export OllamaInstance, Options, initInstance, generate, chat, embed, tags, Tool, ToolFunction, Message
 
 struct OllamaInstance
     url::String
@@ -13,6 +13,9 @@ struct OllamaInstance
         return new(url)
     end
 end
+
+# Options objects
+# ------------
 
 # Sub options object for requests
 Base.@kwdef struct SubOptions
@@ -26,12 +29,68 @@ Base.@kwdef struct SubOptions
     num_predict::Union{Int, Nothing} = nothing
 end
 
+StructTypes.StructType(::Type{SubOptions}) = StructTypes.Struct()
+StructTypes.omitempties(::Type{SubOptions}) = true
+
+# Tool Structs
+# ------------
+
+# Tool object function parameter
+Base.@kwdef struct ToolFunction
+    name::String
+    parameters::Dict{String, Any}
+    description::Union{String, Nothing} = nothing
+end
+
+StructTypes.StructType(::Type{ToolFunction}) = StructTypes.Struct()
+
+# Tool object
+Base.@kwdef struct Tool
+    type::String = "function"
+    func::ToolFunction
+end
+
+StructTypes.StructType(::Type{Tool}) = StructTypes.Struct()
+StructTypes.names(::Type{Tool}) = ((:func, :function),)
+
+# Ollama tool call 
+Base.@kwdef struct ToolCallFunction
+    name::String
+    arguments::Dict{String, Any}
+end
+
+StructTypes.StructType(::Type{ToolCallFunction}) = StructTypes.Struct()
+
+Base.@kwdef struct ToolCall
+    func::ToolCallFunction
+end
+
+StructTypes.StructType(::Type{ToolCall}) = StructTypes.Struct()
+StructTypes.names(::Type{ToolCall}) = ((:func, :function),)
+
+# Message object
+# ------------
+
+# Message object
+Base.@kwdef mutable struct Message
+    role::String
+    content::String
+    images::Union{Vector{String}, Nothing} = nothing
+    tool_calls::Union{Vector{ToolCall}, Nothing} = nothing
+end
+
+# Messages constructor for JSON serialization
+function Messages(messages::Union{Dict, NamedTuple})
+    msgs = JSON3.read(JSON.write(messages), Message) 
+    return msgs
+end
+
 # Universal options struct
-Base.@kwdef struct Options
+Base.@kwdef mutable struct Options
     # Global
     model::String
     stream::Union{Bool, Nothing} = nothing
-    format::Union{Union{String, JSON3.Object}, Nothing} = nothing
+    format::Any = nothing
     # TODO support for dict, JSON input
     options::Union{SubOptions, Nothing} = nothing
     # options::Union{JSON3.Object, Nothing} = nothing
@@ -46,9 +105,9 @@ Base.@kwdef struct Options
     images::Union{Vector{String}, Nothing} = nothing
     # Chat
     # TODO support for dict, JSON input
-    messages::Union{Vector{Dict{String, Any}}, Nothing} = nothing
+    messages::Union{AbstractVector, Nothing} = nothing
     # TODO support for dict, JSON input
-    tools::Union{JSON3.Object, Nothing} = nothing
+    tools::Union{AbstractVector, Nothing} = nothing
     # Embed
     input::Union{String, Nothing} = nothing
     truncate::Union{Bool, Nothing} = nothing
@@ -57,9 +116,6 @@ end
 
 StructTypes.StructType(::Type{Options}) = StructTypes.Struct()
 StructTypes.omitempties(::Type{Options}) = true
-
-StructTypes.StructType(::Type{SubOptions}) = StructTypes.Struct()
-StructTypes.omitempties(::Type{SubOptions}) = true
 
 # Messaging endpoints
 # -----
@@ -73,11 +129,14 @@ function generate(ollama::OllamaInstance, opts::Options)
     return req
 end
 
+# Kwarg handler
+generate(ollama; kwargs...) = generate(ollama, Options(; kwargs...))
+
 # Convenience with kwargs
-function generate(ollama::OllamaInstance; kwargs...)
-    opts = Options(; kwargs...)
-    return generate(ollama, opts)
-end
+# function generate(ollama::OllamaInstance; kwargs...)
+#     opts = Options(; kwargs...)
+#     return generate(ollama, opts)
+# end
 
 # Generate the next chat message in a conversation between a user and an assistant
 function chat(ollama::OllamaInstance, opts::Options)
@@ -88,10 +147,13 @@ function chat(ollama::OllamaInstance, opts::Options)
     return req
 end
 
-function chat(ollama::OllamaInstance; kwargs...)
-    opts = Options(; kwargs...)
-    return chat(ollama, opts)
-end
+# Kwarg handler
+chat(ollama; kwargs...) = chat(ollama, Options(; kwargs...))
+
+# function chat(ollama::OllamaInstance; kwargs...)
+#     opts = Options(; kwargs...)
+#     return chat(ollama, opts)
+# end
 
 # Creates vector embeddings representing the input text
 function embed(ollama::OllamaInstance, opts::Options)
